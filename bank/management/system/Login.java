915ce286b8082429f08cebde609784279774be95
kishang0106R <iamkishansingh27@gmail.com>
2025-05-18 18:48:18 +0530

    new image

--- bank/management/system/Login.java
+++ bank/management/system/Login.java
@@ -1,10 +1,14 @@
 package bank.management.system;
+import java.awt.*;
+
 import javax.swing.*;;
 public class Login extends JFrame {
     Login(){
         setTitle("ATUOMATED TELLER MACHINE");
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/images.png"));
-        JLabel label = new JLabel(i1);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/istockphoto-849792602-612x612.jpg"));
+        Image i2 = i1.getImage(). getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel label = new JLabel(i3);
         add(label);
         setSize(800, 400);
         setVisible(true);
