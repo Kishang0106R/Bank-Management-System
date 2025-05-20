5430128f4ef6c553d391079d27c71a2336a66895
kishang0106R <iamkishansingh27@gmail.com>
2025-05-21 00:48:06 +0530

    add new line

--- bank/management/system/Login.java
+++ bank/management/system/Login.java
@@ -6,17 +6,30 @@ public class Login extends JFrame {
     Login(){
         setTitle("ATUOMATED TELLER MACHINE");
         setLayout(null);
+
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/istockphoto-849792602-612x612.jpg"));
         Image i2 = i1.getImage(). getScaledInstance(150, 150, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);
-        label.setBounds(70, 120, 100, 100);
+        label.setBounds(70, 120, 150, 150);
         add(label);
         getContentPane().setBackground(Color.white);
+
+
         JLabel text = new JLabel("welcome to ATM");
         text.setFont(new Font("Osward", Font.BOLD, 38));
+        text.setBounds(250, 30, 500, 40);
         add(text);
-        text.setBounds(250, 40, 500, 40);
+
+        JLabel cardno = new JLabel("Card NO: ");
+        cardno.setFont(new Font("osward", Font.BOLD, 20));
+        cardno.setBounds(250, 100, 400, 40);
+        add(cardno);
+
+        JLabel pin = new JLabel("PIN:");
+        pin.setFont(new Font("osward", Font.BOLD, 20));
+        pin.setBounds(250, 170, 400, 40);
+        add(pin);
 
         setSize(800, 400);
         setVisible(true);
