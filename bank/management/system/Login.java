67660573f6727e5e7ea5dbe45613abf1b66b2e2b
kishang0106R <iamkishansingh27@gmail.com>
2025-05-20 21:59:16 +0530

    change size of logo

--- bank/management/system/Login.java
+++ bank/management/system/Login.java
@@ -7,16 +7,16 @@ public class Login extends JFrame {
         setTitle("ATUOMATED TELLER MACHINE");
         setLayout(null);
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/istockphoto-849792602-612x612.jpg"));
-        Image i2 = i1.getImage(). getScaledInstance(100, 100, Image.SCALE_DEFAULT);
+        Image i2 = i1.getImage(). getScaledInstance(150, 150, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);
-        label.setBounds(70, 10, 100, 100);
+        label.setBounds(70, 120, 100, 100);
         add(label);
         getContentPane().setBackground(Color.white);
         JLabel text = new JLabel("welcome to ATM");
         text.setFont(new Font("Osward", Font.BOLD, 38));
         add(text);
-        text.setBounds(200, 40, 500, 40);
+        text.setBounds(250, 40, 500, 40);
 
         setSize(800, 400);
         setVisible(true);
