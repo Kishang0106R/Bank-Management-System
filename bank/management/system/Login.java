73b187a9d3c87e60dcab17a6b67291425b613e7f
kishang0106R <iamkishansingh27@gmail.com>
2025-05-21 19:57:11 +0530

    resize

--- bank/management/system/Login.java
+++ bank/management/system/Login.java
@@ -7,28 +7,28 @@ public class Login extends JFrame {
         setTitle("ATUOMATED TELLER MACHINE");
         setLayout(null);
 
-        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/istockphoto-849792602-612x612.jpg"));
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/images.png"));
         Image i2 = i1.getImage(). getScaledInstance(150, 150, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);
-        label.setBounds(70, 120, 150, 150);
+        label.setBounds(70, 100, 150, 150);
         add(label);
         getContentPane().setBackground(Color.white);
 
 
         JLabel text = new JLabel("welcome to ATM");
-        text.setFont(new Font("Osward", Font.BOLD, 38));
+        text.setFont(new Font("Osward", Font.BOLD, 40));
         text.setBounds(250, 30, 500, 40);
         add(text);
 
         JLabel cardno = new JLabel("Card NO: ");
         cardno.setFont(new Font("osward", Font.BOLD, 20));
-        cardno.setBounds(250, 100, 400, 40);
+        cardno.setBounds(250, 130, 400, 40);
         add(cardno);
 
         JLabel pin = new JLabel("PIN:");
         pin.setFont(new Font("osward", Font.BOLD, 20));
-        pin.setBounds(250, 170, 400, 40);
+        pin.setBounds(250, 190, 400, 40);
         add(pin);
 
         setSize(800, 400);
