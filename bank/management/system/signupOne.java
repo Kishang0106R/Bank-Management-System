740a3e404f98e1767e75e51c66c9a25a53218188
kishang0106R <iamkishansingh27@gmail.com>
2025-05-24 21:36:20 +0530

    add back groud

--- bank/management/system/signupOne.java
+++ bank/management/system/signupOne.java
@@ -1,9 +1,15 @@
 package bank.management.system;
+import javax.swing.*;
+import java.awt.*;
 
-public class signupOne {
+public class signupOne extends JFrame {
 
     signupOne(){
-        
+
+        getContentPane().setBackground(Color.white);
+        setSize(850, 800);
+        setLocation(350, 10);
+        setVisible(true);
     }
     public static void main(String[] args) {
         new signupOne();
