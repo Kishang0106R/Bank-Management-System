7964c7a034090078bbe683669daf8d4177595597
kishang0106R <iamkishansingh27@gmail.com>
2025-05-24 21:30:59 +0530

    work on sign up

---
+++ bank/management/system/signupOne.java
@@ -0,0 +1,12 @@
+package bank.management.system;
+
+public class signupOne {
+
+    signupOne(){
+        
+    }
+    public static void main(String[] args) {
+        new signupOne();
+    }
+    
+}
