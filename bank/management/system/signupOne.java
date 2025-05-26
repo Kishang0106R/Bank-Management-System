c9702d9cf175e372e2301966e1d1bb7e607b67b2
kishang0106R <iamkishansingh27@gmail.com>
2025-05-26 23:15:24 +0530

    add name

--- bank/management/system/signupOne.java
+++ bank/management/system/signupOne.java
@@ -11,14 +11,25 @@ public class signupOne extends JFrame {
 
         Random ran = new Random();
         long random = (Math.abs(ran.nextLong() % 9000L) + 1000L);
+
         JLabel formno = new JLabel("APPLICATION FORM NO. " + random);
         formno.setFont(new Font("Raleway", Font.BOLD, 38));
         formno.setBounds(140, 20, 600, 40);
         add(formno);
 
+        JLabel personDetails = new JLabel("page 1: Personal Details");
+        personDetails.setFont(new Font("Raleway", Font.BOLD, 22));
+        personDetails.setBounds(290, 60, 600, 40);
+        add(personDetails);
+
+        JLabel name = new JLabel("Name:");
+        name.setFont(new Font("Raleway", Font.BOLD, 20));
+        name.setBounds(100, 120, 100, 30);
+        add(name);
+
         getContentPane().setBackground(Color.white);
-        setSize(850, 800);
-        setLocation(350, 10);
+        setSize(850, 870);
+        setLocation(350, 0);
         setVisible(true);
     }
     public static void main(String[] args) {
